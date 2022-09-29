22a689ed7a80ca3b6264f4b4694ed8ea79f2d118
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2022-09-29 23:03:02 +0300

    Create HW_3.py

---
+++ HW_3.py
@@ -0,0 +1,9 @@
+x=int (input('введите номер четверти: '))
+if x == 1:
+    print('Х>0 \nY>0')
+elif x==2:
+    print ('Х>0 \nY<0')
+elif x == 3:
+    print ('Х<0 \nY<0')
+elif x == 4:
+    print ('Х<0 \nY>0')
