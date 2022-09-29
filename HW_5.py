72d36e6bcc33fa2d65f0f41660cc644d50aed9f4
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2022-09-29 23:04:23 +0300

    Create HW_5.py

---
+++ HW_5.py
@@ -0,0 +1,7 @@
+print('Введите координаты двух точек')
+x1=float(input('Введите х1: '))
+y1=float(input('Введите y1: '))
+x2=float(input('Введите х2: '))
+y2=float(input('Введите y2: '))
+result_ = round(((x2-x1)**2+(y2-y1)**2)**0.5,3)
+print(result_)
