0732e64a1ee41e62f919f4ee469b2575d652c9d0
kolinrok1 <109362691+kolinrok1@users.noreply.github.com>
2022-10-04 18:11:26 +0300

    Update HW_6.py

--- HW_6.py
+++ HW_6.py
@@ -6,3 +6,17 @@ for i in range(x):
     if True_==True:
         y=int(number_[i])+y
 print(y)
+
+
+number_=int (input('Введите число: '))
+x=[]
+for i in range(number_):
+    x.append(i+1)
+y=0
+while number_!=y:
+    result=1
+    for i in range(y):
+        result=x[i+1]* result
+    print(result)
+    y=y+1
+print()
